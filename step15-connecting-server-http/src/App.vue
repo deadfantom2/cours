c17db799f5d43e931af12c0ea1391524b5101a46
V.SHCHERBOV <v.shcherbov@MacBook-Pro-de-VSHCHERBOV.local>
2019-09-12 12:20:59 +0200

    7

--- step15-connecting-server-http/src/App.vue
+++ step15-connecting-server-http/src/App.vue
@@ -1,28 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" v-model="user.username">
+        </div>
+        <div class="form-group">
+          <label>Email</label>
+          <input type="text" class="form-control" v-model="user.email">
+        </div>
+        <button @click="submit" class="btn btn-primary">Submit</button>
+        <p> {{errors}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      user: {
+        username: '',
+        email: ''
+      },
+      errors: []
+    }
+  },
+  methods: {
+    submit(){
+      console.log(this.user);
+      this.$http.post('https://vuejs-http-3d7e8.firebaseio.com/user.json', this.user)
+              .then((res) => {
+                console.log(res)
+              }, err => {
+                console.log(err.body);
+                this.errors = err.body.error
+              });
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
